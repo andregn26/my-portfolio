82fe08e7f8130eee897d202f6b7db3ff9463bd58
André Gregório <andregn26@gmail.com>
2023-07-13 11:53:59 +0100

    changes in the layout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 import { usePathname } from "next/navigation";
-import { LinkedInIcon } from "./Icons";
+import { GithubIcon, LinkedInIcon } from "./Icons";
+import { motion } from "framer-motion";
 
 const CustomLink = ({ href, title, className = "" }) => {
 	const pathName = usePathname();
@@ -26,26 +27,46 @@ const CustomLink = ({ href, title, className = "" }) => {
 const Navbar = () => {
 	return (
 		<>
-			<header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+			<header className="w-full px-32 py-8 font-medium flex items-center justify-between fixed">
 				<nav>
 					<CustomLink href="/" title="Home" className="mr-4" />
 					<CustomLink href="/about" title="About" className="mx-4" />
 					<CustomLink href="/projects" title="Projects" className="ml-4" />
 				</nav>
 
-				<nav>
-					<Link href="/" target="_blank">
+				<nav className="flex items-center justify-center flex-wrap gap-2">
+					<motion.a
+						href="/"
+						target="_blank"
+						whileHover={{ y: -2 }}
+						whileTap={{ scale: 0.9 }}
+						className="w-6">
 						<LinkedInIcon />
-					</Link>
-					<Link href="/" target="_blank">
+					</motion.a>
+					<motion.a
+						href="/"
+						target="_blank"
+						whileHover={{ y: -2 }}
+						whileTap={{ scale: 0.9 }}
+						className="w-6">
+						<GithubIcon />
+					</motion.a>
+					<motion.a
+						href="/"
+						target="_blank"
+						whileHover={{ y: -2 }}
+						whileTap={{ scale: 0.9 }}
+						className="w-6">
 						T
-					</Link>
-					<Link href="/" target="_blank">
+					</motion.a>
+					<motion.a
+						href="/"
+						target="_blank"
+						whileHover={{ y: -2 }}
+						whileTap={{ scale: 0.9 }}
+						className="w-6">
 						T
-					</Link>
-					<Link href="/" target="_blank">
-						T
-					</Link>
+					</motion.a>
 				</nav>
 				<div className="absolute left-[50%] top-2 translate-x-[-50%]">
 					<Logo />
