93a541d4f9b413e077e805b9cf6f818f85a480f5
andregn26 <andregn26@gmail.com>
2023-12-10 13:13:55 +0000

    added projects to data.js

--- data.js
+++ data.js
@@ -108,7 +108,7 @@ export const projects = [
 		githubLink: "https://github.com/andregn26/GrooveGenius",
 		isLive: true,
 		liveLink: "https://groove-genius.vercel.app/",
-		tags: ["React.js", "JavaScript", "Tailwind CSS", "Daisy UI", "External API", "React Hot Toast"],
+		tags: ["Frontend", "React.js", "JavaScript", "Tailwind CSS", "Daisy UI", "External API", "React Hot Toast"],
 	},
 	{
 		id: 2,
@@ -118,7 +118,7 @@ export const projects = [
 		githubLink: "https://github.com/andregn26/travel-companion-application",
 		isLive: true,
 		liveLink: "https://travel-companion-application.vercel.app/",
-		tags: ["React.js", "Material UI", "Styled Components", "axios", "External API"],
+		tags: ["Frontend", "React.js", "Material UI", "Styled Components", "axios", "External API"],
 	},
 	{
 		id: 3,
@@ -128,7 +128,7 @@ export const projects = [
 		githubLink: "https://github.com/andregn26/appointment-planner",
 		isLive: true,
 		liveLink: "https://contact-planner.vercel.app/contacts",
-		tags: ["React.js", "JavaScript", "Tailwind CSS", "Daisy UI", "React Router", "React Toastify"],
+		tags: ["Frontend", "React.js", "JavaScript", "Tailwind CSS", "Daisy UI", "React Router", "React Toastify"],
 	},
 	{
 		id: 4,
@@ -138,6 +138,26 @@ export const projects = [
 		githubLink: "https://github.com/andregn26/i9-email-eyepeak",
 		isLive: true,
 		liveLink: "https://andregn26.github.io/i9-email-eyepeak/",
-		tags: ["HTML4", "CSS3", "Table Layout", "Litmus", "Outlook Statements"],
+		tags: ["Frontend", "HTML4", "CSS3", "Table Layout", "Litmus", "Outlook Statements"],
+	},
+	{
+		id: 5,
+		name: "Boss Machine (Codecademy Project)",
+		description:
+			"Creation of an entire API to serve information to a Boss Machine, a unique management application for today's most accomplished (evil) entrepreneurs. ",
+		githubLink: "https://github.com/andregn26/CODECADEMY-boss-machine",
+		isLive: false,
+		liveLink: null,
+		tags: ["Backend", "Express.js"],
+	},
+	{
+		id: 6,
+		name: "FlashCards (Codecademy Porject)",
+		description:
+			"Part of the frontend career path of Codecademy. The project served to learn and develop skills using the Redux Toolkit",
+		githubLink: "https://github.com/andregn26/flashcards/tree/main",
+		isLive: false,
+		liveLink: null,
+		tags: ["Frontend", "React.js", "JavaScript", "Redux Toolkit", "Tailwind CSS", "Daisy UI"],
 	},
 ];
